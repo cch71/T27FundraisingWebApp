e0adf77e994e8fed278dd5537d89c8038cbc83e3
Craig Hamilton <cch71a@gmail.com>
2020-10-18 01:08:01 -0500

    Adding printout of all users function

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -21,9 +21,20 @@ var WildRydes = window.WildRydes || {};
 
     userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
+
     if (typeof AWSCognito !== 'undefined') {
         AWSCognito.config.region = _config.cognito.region;
     }
+
+    userPool.client.listUsers({
+        'UserPoolId': _config.cognito.userPoolId
+    }, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else {
+            console.log("User Data:");
+            console.log(data);           // successful response
+        }
+    });
 
     WildRydes.signOut = function signOut() {
         userPool.getCurrentUser().signOut();
