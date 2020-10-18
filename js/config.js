1f03a38a1f84a9729395cd2e7cf3ead1eb370f1d
Craig Hamilton <cch71a@gmail.com>
2020-10-17 23:50:14 -0500

    Updating config with lambda api

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://j0azby8rm6.execute-api.us-east-1.amazonaws.com/prod'
     }
 };
