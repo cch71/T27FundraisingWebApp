04c236006783ccd1b395672e540a1282c107d579
Craig Hamilton <cch71a@gmail.com>
2020-12-27 20:44:06 -0600

    Adding poppler back in aws try #4

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,8 +2,8 @@ import "./src/style/index.scss"
 
 // Only need jquery/popper for bootstrap MODAL and now DataTables TODO: Work on non bootrap model
 // and do away with this
-// import 'jquery/dist/jquery.min.js'
-// import 'popper.js/dist/popper.min'
+import 'jquery/dist/jquery.min.js'
+import 'popper.js/dist/popper.min'
 
 // Needed for bootstrap components
 //import 'bootstrap/dist/js/bootstrap.min.js'
