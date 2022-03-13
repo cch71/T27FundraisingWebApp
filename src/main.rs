362990127b8d9c22948f21c5b833aa3abd6771d0
Craig Hamilton <cch71a@gmail.com>
2022-03-13 01:15:28 -0600

    Updating some components and adding ability to log AWS Commit when deployed

--- src/main.rs
+++ src/main.rs
@@ -295,5 +295,6 @@ impl Component for Model {
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
+    log::info!("RelVer: {}", std::option_env!("AWS_COMMIT_ID").unwrap_or("?"));
     yew::start_app::<Model>();
 }
