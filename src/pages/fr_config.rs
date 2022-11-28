c76bd5991ac208e58470214ea6f88ce53910e2f3
Craig Hamilton <cch71a@gmail.com>
2022-11-27 23:30:03 -0600

    Created reset order data UI

--- src/pages/fr_config.rs
+++ src/pages/fr_config.rs
@@ -62,6 +62,38 @@ fn set_spreading_cost() -> Html
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
+#[function_component(ResetOrders)]
+fn reset_orders_database() -> Html
+{
+    let on_reset_db = {
+        move |_evt: MouseEvent| {
+            let do_reset_resp = gloo::dialogs::prompt(
+                "This will remove all order data from the system.\nIT IS DESTRUCTIVE!!!\nAre You Sure?\nType \"delete order data\" to delete",
+                None);
+            log::info!("Resetting Order Database: {}", do_reset_resp.map_or(false, |v| v=="delete order data"));
+        }
+    };
+
+    html! {
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">
+                    {"Clear Order Data"}
+                </h5>
+               <div class="row">
+                  <button class="btn btn-primary" onclick={on_reset_db}>
+                      <span class="spinner-border spinner-border-sm me-1" role="status"
+                      aria-hidden="true" id="resetOrderTableSpinner" style="display: none;" />
+                      {"Save"}
+                  </button>
+               </div>
+            </div>
+        </div>
+    }
+}
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
 
 #[function_component(FrConfig)]
 pub fn fr_config() -> Html
@@ -77,6 +109,7 @@ pub fn fr_config() -> Html
                 <div class="col-xs-1 d-flex justify-content-center">
                     <SpreadingCost/>
                     <DeliveryUl/>
+                    <ResetOrders/>
                 </div>
             </div>
             <div class="row">
